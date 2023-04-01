1d0f9ed52ff2555c446b2776867bf8bd9f9df963
TBill06 <bill.tushar2@gmail.com>
2023-04-01 02:21:12 -0230

    Navbar done

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ function App() {
   return (
     <div className="bg-gray-50 min-w-screen min-h-screen">
       <header>
-        <nav className="">
+        <nav className="fixed top-0 w-full z-10 backdrop-blur bg-white/50">
           <ul className="flex justify-between items-center h-full py-5 mx-20">
             <li className="text-emerald-900 font-bold ">Logo</li>
+            <div>
+            <li className="text-base inline px-3 hover:underline decoration-2 underline-offset-[6px]">Text1</li>
+            <li className="text-base inline px-3 hover:underline decoration-2 underline-offset-[6px]">Text2</li>
+            <li className="text-base inline px-3 hover:underline decoration-2 underline-offset-[6px]">Text3</li>
+            </div>
             <li className="font-bold bg-black text-sm  text-gray-50 px-3 py-1">
               Contact us
             </li>
           </ul>
         </nav>
       </header>
-      <body className="flex justify-center flex-col">
+      <body className="pt-[68px] pt flex justify-center flex-col">
         <section
           id="intro"
           className="min-h-screen mb-5 mx-5 bg-emerald-900 rounded-lg flex items-center justify-center flex-col"
